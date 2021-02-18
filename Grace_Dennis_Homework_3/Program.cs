5b2f04e2c3b2d175abb6ed381dbc96f19bc9e70c
Grace Dennis <G.Dennis@ou.edu>
2021-02-18 16:07:14 -0600

    started

---
+++ Grace_Dennis_Homework_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace_Dennis_Homework_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
---
+++ Grace_Dennis_Homework_3/Trapezoid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace_Dennis_Homework_3
+{
+    class Trapezoid
+    {
+    }
+}
